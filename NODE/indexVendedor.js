929137e21c3a0f5f88ea56128c312261423acff7
RaianiYohana <raianirescarolli@gmail.com>
2024-06-21 07:50:48 -0700

    Criação de tudo e get do vendedor

--- NODE/indexVendedor.js
+++ NODE/indexVendedor.js
@@ -11,3 +11,20 @@ const PORTA = 3000
 app.listen( PORTA, function(){
   console.log("Servidor iniciados na porta" +PORTA)  
 })
+
+app.get("/vendedor/", function (req,res){
+  res.send( vendedor )
+})
+
+app.get("/vendedor/:id", function(req, res){
+  var vendedorEncontrado = vendedor.find( function( vendedorAtual ){
+    return vendedorAtual.id == parseInt(req.params.id)
+  })
+  if( !vendedorEncontrado ){
+    res.status( 404 ).send({})
+  } else {
+    res.send( vendedorEncontrado)
+  }
+})
+
+
