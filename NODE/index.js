02ae3c27c9d6aa789ebc7e179cd7bfca98205679
RaianiYohana <raianirescarolli@gmail.com>
2024-07-31 20:35:53 -0700

    Update index.js

--- NODE/index.js
+++ NODE/index.js
@@ -1,16 +1,18 @@
-const banco = require("./banco")
-const express = require('express')
-const vendedor = require("./vendedor")
-const pneus = require("./pneus")
-const { where } = require("sequelize")
+const banco = require("./banco") // importa o módulo banco que lida com a conexão e operação do banco de dados
+const express = require('express') // importa o módulo express que é um framework web para Node.js, que simplifica a criação de servidores web
+const vendedor = require("./vendedor") // importa o módulo vendedor onde contêm definições específicas do modelo e funções relacionadas
+const pneus = require("./pneus") // importa o módulo pneus, onde contêm definições específicas do modelo e funções relacionadas
+const { where } = require("sequelize") // importa o método "where" do sequelize para consultas específicas
 
-const app = express()
-app.use(express.json())
+const app = express() //  faz a criação de uma instância da aplicação Express
+app.use(express.json()) // Configura o middleware para processar requisições com corpo JSON
 
 
-banco.conexao.sync( function(){
-  console.log("Banco de dados conectado.");
+banco.conexao.sync( function(){  // faz a sincronização com o banco de dados
+  console.log("Banco de dados conectado."); // mensagem para confirmar a conexão
 })
+
+//Aqui é definido cabeçalhos para permitir requisições de qualquer origem, onde possui métodos e cabeçalhos específicos 
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -18,20 +20,17 @@ app.use(function (req, res, next) {
   next();
 });
 
-
+// Aqui mostra a definição da porta do servidor e inicia o servidor. Contudo, tem uma mensagem de confirmação.
 const PORTA = 3000 
 app.listen( PORTA, function(){
   console.log("Servidor iniciados na porta" +PORTA)  
 })
 
-//1 - mostra o vendedor
-app.get("/vendedor/", async function (req,res){
-  const resultado = await vendedor.vendedor.findAll()
-res.send(resultado);
-})
+//1 -
 
-app.post("/vendedor/", async function( req, res){
-  const resultado = await vendedor.vendedor.create({
+// Cria um novo vendedor com base nos dados fornecidos no corpo da requisição e envia a resposta
+ app.post("/vendedor/", async function( req, res){
+  const resultado = await vendedor.vendedor.create({ //criar
     nome:req.body.nome,
     cnpj:req.body.cnpj
 
@@ -39,8 +38,15 @@ app.post("/vendedor/", async function( req, res){
   res.send(resultado)
 });
 
+// Busca todos os vendedores e envia a resposta
+app.get("/vendedor/", async function (req,res){  
+  const resultado = await vendedor.vendedor.findAll() //encontrar tudo
+res.send(resultado);
+})
+
+// Atualiza os dados de um vendedor com base no ID e os dados do corpo da requisição. Envia o vendedor atualizado
 app.put("/pessoa/:id", async function(req, res){
-  const resultado = await vendedor.vendedor.update({
+  const resultado = await vendedor.vendedor.update({ //atualizar
     nome:req.body.nome,
     cnpj:req.body.cnpj
   }, {
@@ -50,12 +56,13 @@ app.put("/pessoa/:id", async function(req, res){
   if(resultado == 0){
     res.status(404),send({})
   } else{
-    res.send(await vendedor.vendedor.findByPk(req.params.id))
+    res.send(await vendedor.vendedor.findByPk(req.params.id)) // encontrar por primary key
   }
 })
 
+// Faz a exclusão de um vendedor com base no id. Se for excluido envia um status 204, senão um de 404 casos não for encontrado
 app.delete("/vendedor/:id", async function(req, res){
-  const resultado = await vendedor.vendedor.destroy({
+  const resultado = await vendedor.vendedor.destroy({ //destruir
     where:{
       id:req.params.id
     }
@@ -68,6 +75,7 @@ app.delete("/vendedor/:id", async function(req, res){
 })
 
 //2- vendedor
+// Pega um vendedor pelo seu id e inclui seus pneus relacionados a ele
 app.get("/vendedor/:id", async function(req, res){
  const vendedorSelecionado = await vendedor.vendedor.findByPk(req.params.id,
 { include: {model: pneus.pneus}}
@@ -80,6 +88,7 @@ if( vendedorSelecionado == null){
 })
 
 //questão 3 de subconjunto (Vendedor para pneu)
+//Busca vendedor pelo nome e inclui seus pneus relacionados 
 app.get("/vendedor/nome/:nome", async function(req,res){
   const vendedorSelecionado = await vendedor.vendedor.findAll(
     {
@@ -87,7 +96,6 @@ app.get("/vendedor/nome/:nome", async function(req,res){
       where: { nome:req.params.nome}
     }
   ) 
-
 if ( vendedorSelecionado == null){
   res.status(404).send({})
 } else {
@@ -96,6 +104,7 @@ if ( vendedorSelecionado == null){
 })
 
 //4 - vendedor
+// cria um novo vendedor com base nos dados fornecidos e inclui o Id do pneu
 app.post("/vendedor/", async function (req,res){
   const resultado = await vendedor.vendedor.create(
     {
@@ -108,6 +117,7 @@ res.send(resultado)
 
 
 //5
+//Atualiza um vendedor pelo id 
 app.put("/vendedor/:id", async function(req,res){
   console.log(req.params.id)
   const resultado = await vendedor.vendedor.update({
@@ -126,6 +136,7 @@ app.put("/vendedor/:id", async function(req,res){
 })
 
 //6
+// deleta o vendedor pelo seu id 
 app.delete("/vendedor/:id",async function(req,res){
   const resultado = await vendedor.vendedor.destroy({
       where:{
@@ -142,13 +153,14 @@ app.delete("/vendedor/:id",async function(req,res){
 // Código da parte dos pneus
 
 //questao 1 Ler todos os registros da entidade A e B (1 ponto).
-
+//Busca todos os pneus inseridos
 app.get("/pneus/", async function (req,res){    /**mostra os pneus */
     const resultado = await pneus.pneus.findAll()
     res.send( resultado )
 })
 
 /**Questao 2  */
+// Busca todos os pneus inseridos pelo id e inclui o nome dos pneus relacionados.
 app.get("/pneus/:id", async function(req,res){  
     const pneuEncontrado = await pneus.pneus.findByPk(req.params.id,
       {include: {model: vendedor.vendedor}}
@@ -162,6 +174,7 @@ app.get("/pneus/:id", async function(req,res){
 
 /*questao 3 Ler subconjunto de registros, buscando por um atributo da entidade A e B (3 pontos). E ́
 necess ́ario ao ler A, buscar todos os registros de B vinculados com ele e vice-versa.*/
+// Busca todos os pneus pelo nome e os vendedores que estão relacionados a ele
 app.get("/pneu/nome/:nome" , async function (req, res){
   const pneusEscolhido = await pneus.pneus.findAll(
     {
@@ -176,8 +189,8 @@ app.get("/pneu/nome/:nome" , async function (req, res){
   }
 })
 
-
 /**4-Criar um registro da entidade A e B (2 pontos). E necess ́ario criar o v ́ınculo entre a entidade A e B. */
+// cria um novo registro do pneu  
 app.post("/pneus/", async function( req, res ){  
     const novoPneu = await pneus.pneus.create({
       nome: req.body.nome,
@@ -190,6 +203,7 @@ app.post("/pneus/", async function( req, res ){
   })
 
 /**5-Atualizar um registro da entidade A e B (2 pontos). E necess ́ario criar o v ́ınculo entre a entidade A e B*/
+// Atualiza um novo pneu que já existe pelo id do mesmo
 app.put("/pneus/:id", async function(req,res){
   const resultado = await pneus.pneus.update({
     nome:req.body.nome,
@@ -206,6 +220,7 @@ app.put("/pneus/:id", async function(req,res){
 })
 
 /**6-Excluir um registro da entidade A e B (1 ponto). */
+//Exclui um pneu pelo id
 app.delete("/pneus/:id",async function(req,res){
   const resultado = await pneus.pneus.destroy({
       where:{
